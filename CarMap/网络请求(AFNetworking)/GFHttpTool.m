ff7ef7b71d9ee64361ff4c240e71ea93a9ad5263
inCarGithub <363846892@qq.com>
2016-03-17 09:31:28 +0800

    网络请求

---
+++ CarMap/网络请求(AFNetworking)/GFHttpTool.m
@@ -0,0 +1,47 @@
+//
+//  GFHttpTool.m
+//  GFHttpTool(AFNetworking)
+//
+//  Created by 陈光法 on 15/12/15.
+//  Copyright © 2015年 陈光法. All rights reserved.
+//*** 网络请求 ****
+
+#import "GFHttpTool.h"
+#import "AFNetworking.h"
+
+@implementation GFHttpTool
+
++ (void)get:(NSString *)url parameters:(NSDictionary *)parameters success:(void(^)(id responseObject))success failure:(void(^)(NSError *error))failure {
+
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    [manager GET:url parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if(success) {
+            success(responseObject);
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        if(failure) {
+            failure(error);
+        }
+    }];
+}
+
+
+
++ (void)post:(NSString *)url parameters:(NSDictionary *)parameters success:(void(^)(id responseObject))success failure:(void(^)(NSError *error))failure {
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    [manager POST:url parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if(success) {
+            success(responseObject);
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        if(failure) {
+            failure(error);
+        }
+    }];
+
+}
+
+@end
